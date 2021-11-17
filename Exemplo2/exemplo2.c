a49fe12f71a6eb1c0282df74036fbf2967489be2
PauloHoashi <62997824+pauloHoashi@users.noreply.github.com>
2021-11-17 12:09:32 -0300

    Create exemplo2.c
    Arquivo fonte do exemplo2.

---
+++ Exemplo2/exemplo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+   int e_numero_1, e_numero_2, s_resultado;
+   
+   scanf("%i", &e_numero_1);
+   scanf("%i", &e_numero_2);
+   s_resultado = e_numero_1 + e_numero_2;
+   printf("%d\n", s_resultado);
+   
+   return 0;
+}
